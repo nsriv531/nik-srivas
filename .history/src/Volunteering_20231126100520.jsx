83f8e35737016449a41c704c4a20656e4b914129
nashdashin <srivastavanikhil226@yahoo.ca>
2023-11-26 10:06:53 -0700

    changes

--- .history/src/Volunteering_20231126100520.jsx
+++ .history/src/Volunteering_20231126100520.jsx
@@ -4,26 +4,29 @@ function Volunteering() {
 
     return (
         <div className="Volunteering">
-        <h1 id="projects">Volunteering and Extra</h1>
+        <h1 id="projects">Volunteering and Extracurricular Activities</h1>
         <h2>AC Robotics and Game Development</h2>
         <div className="ProjectContainer">
           <div className="Project">
             <div>
-              <h2>Director of Education - AC Robotics
+              <h2>Director of Education for AC Robotics
               </h2>
               <p>
-                At AC Robotics, I lead the entire education team for the non-profit organization of AC Robotics.
-                We're an organization that has extended into all post secondary schools here in Calgary. I run all our teams in
-                the name of First Robotics. As of last year, our first FTC reached the provincial championships, where we placed 
-                top 20 out of 50 teams that competed.
+                I'm a board member at AC Robotics, a non-for-profit organization that is about teaching youth and post-secondary about robotics. I lead the entire education team that consist of Post-Secondary students,
+                Where we run three different FIRST Robotics teams.  As of last year, our First Tech Challenge Team, Neo Robotics, reached the provincial championships in Red Deer of 2023, where we placed 
+                20th out of 50 teams that competed. I also help the organization run STEM related challenges and competitions that revolve around educating the youth and freshman
+                post-secondary students.
                 </p>
-              <div className="ProjectTags">
+              <div className="VolunteeringTags">
                 <div>Leadership</div>
                 <div>Creativity</div>
-                <div>Education</div>
+                <div>Teaching</div>
+                <div>Teamwork</div>
+                <div>Engineering</div>
+                <div>Event Planning</div>
                 <div>Java</div>
               </div>
-              <a class="ProjectLink" href="https://www.acrobotics.ca/first">
+              <a class="VolunteeringLink" href="https://www.acrobotics.ca">
                 View Project
               </a>
             </div>
@@ -38,12 +41,13 @@ function Volunteering() {
                I was able to facilitate a great environment for those wanting to learn game development. This was done through my efforts in hosting events, such
                as game jams, social meet ups, as well as the collaboration with other organizations and clubs throughout calgary and the alberta region. 
               </p>
-              <div className="ProjectTags">
+              <div className="VolunteeringTags">
                 <div>Leadership</div>
                 <div>Game Development</div>
                 <div>Event Handling</div>
+                <div>Teamwork</div>
               </div>
-              <a class="ProjectLink" href="https://mru-gddc.vercel.app/">
+              <a class="VolunteeringLink" href="https://mru-gddc.vercel.app/">
                 View Project
               </a>
             </div>
