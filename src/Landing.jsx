9e8de8c60a53e21155b95c95f3c84255d5f54ed4
nsriv531 <nsriv531@mtroyal.ca>
2023-11-28 21:47:43 -0700

    sdfsdfa

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -5,7 +5,7 @@ function Landing() {
     <div className="Landing">
       <h1>Hi, I'm Nik.</h1>
       <h2>
-        I'm a Software Engineer, Game Developer, and a student at Mount Royal University.
+        I'm a Software Engineer, Game Developer, and a leader in STEM education.
       </h2>
       <div className="LandingButton">Contact Me</div>
     </div>
