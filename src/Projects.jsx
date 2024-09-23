d646ef053ba9687f956d36a6bec61ac299bc9d1a
nsriv531 <nsriv531@mtroyal.ca>
2024-09-23 14:12:23 -0600

    tensor flow project added

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -105,6 +105,15 @@ function Projects() {
         projectimage="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Pandas_logo.svg/2560px-Pandas_logo.svg.png"
       />
       
+
+      <Project
+        title = "TensorFlow Project"
+        description = "This is a personal project of mine where I got familar with the TensorFlow ML Framework."
+        tags={["Python", "TensorFlow"]}
+        link="https://github.com/nsriv531/TensorFlow-Personal-Project"
+        projectimage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEsqbuvRgmIsxTT1R_bCdv8txFKkw2ylx5Lg&s"
+      />
+
       <Project
         title = "Assignment 1 - COMP 4522 (Advanced Databases)"
         description = "For my advanced database class, I implemented a Roll-Back system using Python. This assignment simulates a roll-back system with the use of a CSV file upon execution of the script. I did this assignment in a team of two which helped foster my skills in teamwork and collaboration."
