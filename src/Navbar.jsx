0b34fa42a3f719170c9530fcf3d42ceaadf2fb35
nsriv531 <nsriv531@mtroyal.ca>
2024-11-11 14:57:02 -0700

    yo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,11 +3,11 @@ import "./App.css";
 function Navbar() {
   return (
     <div className="Navbar bg-gradient-to-r from-lavender-pink to-light-cyan">
-      <div className="Navbarlogo">
+      <div className="Navbarlogo text-5xl font-bold mb-4 text-gray-800">
         <h4>Nik</h4>
         <h5>Software Developer</h5>
       </div>
-      <div className="Navbarlinks">
+      <div className="Navbarlinks text-gray-800">
         <a href="#landing">Home</a>
         <a href="#projects">Projects</a>
         <a href="#hireme">Hire Me</a>
