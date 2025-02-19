03ea7d75fe408ec6f594d017e95666332aeb75de
nsriv531 <nsriv531@mtroyal.ca>
2025-02-18 21:51:36 -0700

    window location

--- src/ResumeButton.jsx
+++ src/ResumeButton.jsx
@@ -1,15 +1,14 @@
 function ResumeButton() {
-    return (
-      <a 
-        href="/public/resume.pdf" 
-        target="_blank" 
-        rel="noopener noreferrer"
-        className="text-blue-600 hover:underline"
-      >
-        My Resume
-      </a>
-    );
-  }
-  
-  export default ResumeButton;
-  
+  return (
+    <a 
+      href={`${window.location.origin}/resume.pdf`} 
+      target="_blank" 
+      rel="noopener noreferrer"
+      className="text-blue-600 hover:underline"
+    >
+      My Resume
+    </a>
+  );
+}
+
+export default ResumeButton;
